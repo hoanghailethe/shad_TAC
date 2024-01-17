3a3e8b127a04f813ac8a4b960e7a336c409e10e0
Hai Hoang <lethehoanghai@gmail.com>
2024-01-18 00:18:57 +0700

    learning FE

---
+++ src/components/MyComponent/Nav/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+
+const Nav = () => {
+  return (
+    <div>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+    </div>
+  )
+}
+
+export default Nav
