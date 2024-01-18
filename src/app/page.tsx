72d7e1998eb7251bc45960a907cc2220ad4bda97
hoanghailethe <hoanghailethe@users.noreply.github.com>
2024-01-18 13:30:31 +0700

    navbar layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 export default function Home() {
   return (
     <main className="flex">
-      
       MAIN 
     </main>
   )
--- src/components/MyComponent/Nav/page.tsx
+++ src/components/MyComponent/Nav/page.tsx
@@ -14,18 +14,18 @@ import {
 
 const Nav = () => {
   return (
-    <div>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-
+    <div className="flex justify-between padding">
+      <div> TACCOM </div>
+      <div> 
+        <ul className="flex flex-grow justify-end gap-6">
+          <li>HOME</li> 
+          <li>Dự án </li>  
+          <li>Dịch vụ</li>  
+          <li>Liên hệ</li>  
+          <li>Tuyển dụng</li>   
+          <li>Blogs</li>   
+        </ul>   
+      </div>
     </div>
   )
 }
